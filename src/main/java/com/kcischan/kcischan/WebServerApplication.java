b8b464fd3441cd23bc7a86e493ee1e2b4c2cce3b
idadwind1 <idadwind@gmail.com>
2025-05-27 17:55:24 +0800

    fix

--- src/main/java/com/kcischan/kcischan/WebServerApplication.java
+++ src/main/java/com/kcischan/kcischan/WebServerApplication.java
@@ -3,7 +3,6 @@ package com.kcischan.kcischan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @SpringBootApplication
 public class WebServerApplication {
